7baa7ca94a0a7a2bb2da44926d328d57cca1d944
ThucTungNguyen <thuctung@hexagonasia.co>
2021-01-24 21:33:44 +0700

    factorial function

--- Alrogithm/Factorial.js
+++ Alrogithm/Factorial.js
@@ -1,8 +1,8 @@
-const tinhGiaiThua = (n) => {
+const factorial = (n) => {
   if (n === 1) {
     return n;
   }
-  return n * tinhGiaiThua(n - 1);
+  return n * factorial(n - 1);
 };
 
 const useLoop = (n) => {
